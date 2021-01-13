a88b8adbe81ccb917b2605a503455cba78dbee43
ferhatchala <ferhatchala2016@gmail.com>
2021-01-13 23:43:16 +0100

    Function Based API Views

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -1,19 +1,45 @@
 from rest_framework import serializers
 from .models import Article
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=100)
-    email = serializers.CharField(max_length=100)
-    date = serializers.DateTimeField()
+#Model Serializer 
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id','title','author']
 
-    def create(self,validated_data):
-        return Article.objects.create(validated_data)
 
-    def update(self,instance,validated_data):
-        instance.title = validated_data.get('title',instance.title)
-        instance.author = validated_data.get('author',instance.author)
-        instance.email = validated_data.get('email',instance.email)
-        instance.date = validated_data.get('date',instance.date)
-        return instance
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Serilaiser 
+#class ArticleSerializer(serializers.Serializer):
+#    title = serializers.CharField(max_length=100)
+#    author = serializers.CharField(max_length=100)
+#    email = serializers.CharField(max_length=100)
+#    date = serializers.DateTimeField()
+#
+#    def create(self,validated_data):
+#        return Article.objects.create(validated_data)
+#
+#    def update(self,instance,validated_data):
+#       instance.title = validated_data.get('title',instance.title)
+#       instance.author = validated_data.get('author',instance.author)
+#       instance.email = validated_data.get('email',instance.email)
+#       instance.date = validated_data.get('date',instance.date)
+#       return instance
+#
